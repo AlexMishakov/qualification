4b4db21378306eae76594f2126706af87e5e030c
Alex Mishakov <sasha.mishakov@gmail.com>
2019-05-21 02:13:33 +0300

    animation cell in category

--- pro-service/Controller/CategoryViewController/CategoryViewController.m
+++ pro-service/Controller/CategoryViewController/CategoryViewController.m
@@ -60,4 +60,9 @@
     return cell;
 }
 
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    [tableView deselectRowAtIndexPath:indexPath animated:YES];
+}
+
 @end
