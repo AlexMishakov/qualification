34434c713ade7bf594cc2a9984aa679505dc29d0
Alex Mishakov <sasha.mishakov@gmail.com>
2019-05-21 04:01:31 +0300

    Alex Mishakov
    edit model event

--- model/Event.php
+++ model/Event.php
@@ -7,12 +7,21 @@
 			$Array = array();
 			$ArrayEvents = array();
 			$idEvent = $req['id'];
+			$Today = $req['today'];
 			
 			if (!empty($idEvent))
 			{
 				$sqlForId = "create_event_event.id = '$idEvent' AND";
 			}
 			
+			if ($Today == true)
+			{
+				$dateToday = date('Y-m-d H:i:s');
+				$dateTomorrow = date('Y-m-d 00:00:00', strtotime("+1 day"));
+				$sqlToday = "(create_event_event.created_date > '$dateToday' AND create_event_event.created_date < '$dateTomorrow') AND";
+			}
+			
+			// TODO: tags
 			$sql = "SELECT
 						create_event_event.id,
 						create_event_event.title,
@@ -27,19 +36,21 @@
 						
 						create_event_organization.title as organization_title,
 						
-						create_event_profile.name as profile_name,
-						create_event_profile.surname as profile_surname
+						auth_user.first_name as profile_name,
+						auth_user.last_name as profile_surname
 					FROM
 						create_event_event,
 						create_event_agerating,
 						create_event_organization,
-						create_event_profile
+						auth_user
 					WHERE
 						$sqlForId
+						$sqlToday
 						create_event_agerating.id = create_event_event.age_rating_id AND
 						create_event_event.status = 1 AND
 						create_event_organization.id = create_event_event.id_venue_id AND
-						create_event_profile.id = create_event_event.id_author_id";
+						auth_user.id = create_event_event.id_author_id
+					ORDER BY create_event_event.created_date";
 			$result = mysqli_query($this->conn, $sql);
 			
 			if (mysqli_num_rows($result) > 0)
